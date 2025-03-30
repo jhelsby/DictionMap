7df1a4ee0dcb213abe185cbec530930e27035c5e
jhels <11036537+jhels@users.noreply.github.com>
2025-03-30 17:51:53 +0200

    Maps a category to all associated words, ordered by priority

--- categoriser.py
+++ categoriser.py
@@ -1,17 +1,32 @@
 import numpy as np
+from collections import defaultdict
 
 categories_per_word = 3
 
+# Categorise all words.
 def categoriser():
   words_list, words_vectors = load_embeddings_file('words.vec')
   categories_list, categories_vectors = load_embeddings_file('categories.vec')
 
-  for word, vector in zip(words_list, words_vectors):
+  # Maps a category to all associated words, ordered in the tuple by priority.
+  # categories["some_category"] = ([the most linked words], [the secondmost linked words],
+  #                               ... [the n-most linked]).
+  categories = defaultdict(lambda: tuple([] for _ in range(categories_per_word)))
+
+  for np_word, vector in zip(words_list, words_vectors):
+
+    word = str(np_word)
+
     similarities = cosine_similarities(vector, categories_vectors)
     most_similar_idx = np.argsort(similarities)[::-1]
 
     most_similar_categories = [[categories_list[i]] for i in most_similar_idx[:categories_per_word]]
-    print(f'{word}:  \t{" ".join(str(category[0]) for category in most_similar_categories)}')
+
+    for i in range(len(most_similar_categories)):
+      curr_category = str(most_similar_categories[i][0])
+      categories[curr_category][i].append(word)
+
+  return categories
 
 # Function to compute cosine similarity between a vector and a list of word vectors.
 def cosine_similarities(query_vector, vectors):
